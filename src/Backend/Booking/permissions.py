b50c9d98437feb9291ac532590e328c3b136ee31
Van Tang <167090977+vanhjhj@users.noreply.github.com>
2024-12-24 19:14:47 +0700

    Revert "Add modal general"

--- src/Backend/Booking/permissions.py
+++
@@ -1,5 +0,0 @@
-from rest_framework.permissions import BasePermission
-
-class IsEmployeeOrAdmin(BasePermission):
-    def has_permission(self, request, view):
-        return request.user.is_authenticated and (request.user.account_type == 'Employee' or request.user.is_staff)
--- src/Backend/Promotion/serializers.py
+++
@@ -1,7 +0,0 @@
-from rest_framework import serializers
-from .models import Promotion
-
-class PromotionSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Promotion
-        fields = '__all__'
